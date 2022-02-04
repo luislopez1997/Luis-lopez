b257f7082f97f391b1785d161907c1effa955af1
luislopez1997 <99003712+luislopez1997@users.noreply.github.com>
2022-02-04 07:44:06 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 let btn_amburguesa = document.getElementById('btn_amburguesa');
-let nav = document.querySelector('nav');
+let barra_navegacion = document.querySelector('.barra_navegacion');
 
 document.addEventListener('click',(e)=>{
     if(e.target.matches('#btn_amburguesa')||e.target.matches('.amburguesa') ){
-        nav.classList.toggle('toggle_btn_amburguesa');
+        barra_navegacion.classList.toggle('toggle_btn_amburguesa');
     }
-    if(e.target.matches('nav')){
-        nav.classList.toggle('toggle_btn_amburguesa');
+    if(e.target.matches('a')){
+         barra_navegacion.classList.toggle('toggle_btn_amburguesa');
     }
 });
