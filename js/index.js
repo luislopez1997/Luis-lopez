c93a6aa40485b0908262099cb580d0930d4847a6
luislopez1997 <99003712+luislopez1997@users.noreply.github.com>
2022-02-07 22:51:33 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,11 +1,33 @@
 let btn_amburguesa = document.getElementById('btn_amburguesa');
 let barra_navegacion = document.querySelector('.barra_navegacion');
-
-document.addEventListener('click',(e)=>{
-    if(e.target.matches('#btn_amburguesa')||e.target.matches('.amburguesa') ){
-        barra_navegacion.classList.toggle('toggle_btn_amburguesa');
-    }
-    if(e.target.matches('a')){
-         barra_navegacion.classList.toggle('toggle_btn_amburguesa');
-    }
+let formulario_contacto_enviar = document.getElementById('formulario_contacto_enviar');
+/*conexion y desconexion a internet*/
+var div = document.createElement('div');
+var conectado = '<span>Estas conectado a internet<span/>';
+var desconectado = '<span>No tienes conexión a internet<span/>';
+var conexion_internet = document.querySelector('.conexion_internet');
+document.body.appendChild(div);
+formulario_contacto_enviar.addEventListener('click',(e)=>{
+   // e.preventDefault();
+});
+btn_amburguesa.addEventListener('click',(e)=>{
+    barra_navegacion.classList.toggle('toggle_btn_amburguesa');
+});
+/*Desconectado de internet*/
+window.addEventListener('offline',()=>{
+    div.innerHTML = desconectado;
+    div.classList.add('conexion_internet');
+    setTimeout(() => {
+        div.classList.remove('conexion_internet');
+    }, 3000);
+});
+/*Conectado de internet*/
+window.addEventListener('online',()=>{
+    div.innerHTML = conectado;
+    div.classList.add('conexion_internet');
+    div.style.background = 'green';
+    setTimeout(() => {
+        div.classList.remove('conexion_internet');
+        div.style.background = 'red';
+    }, 3000);
 });
